29dec07a370db4ed2be5109df2eb8447ee7084af
dharanibalasubramaniam <dharanisuburamani@gmail.com>
2016-06-07 19:08:41 +0530

    Create factt.c

---
+++ factt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int f=1,i,n;
+  printf("Enter the number");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    f=f*i;
+  }
+  printf("Factorial is %d",f);
+  return 0;
+}
