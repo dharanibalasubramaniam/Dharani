bdf1ccb7654ffa74d347d35e0426b8c36e697c6c
dharanibalasubramaniam <dharanisuburamani@gmail.com>
2016-06-08 10:05:00 +0530

    Rename prime2.c to prime1.c

---
+++ prime1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,i,j,flag=0;
+  scanf("%d%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+  flag=0;
+    for(j=2;j<i;j++)
+    {
+      if(i%j==0)
+      {
+        flag=1;
+        break;
+      }
+    }
+    if(flag==0)
+    printf("%d",i);
+  }
+  return 0;
+}
+  
+  
