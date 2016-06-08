30318b7a4428e9c46cf0128b7b356679844d99dd
dharanibalasubramaniam <dharanisuburamani@gmail.com>
2016-06-08 09:26:32 +0530

    Create odd2.c

---
+++ odd2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,i;
+  printf("Enter the intervals:");
+  scanf("%d%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+    if(i%2!=0)
+    {
+      printf("%d",i);
+    }
+  }
+  return 0;
+}
